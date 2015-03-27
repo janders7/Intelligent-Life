f78b21cdb1dad2f847a081c7ad118ce2b02487df
agguzman <a.guillermoguzman@gmail.com>
2015-03-27 12:31:57 -0400

    second commits

---
+++ Puzzle.java
@@ -0,0 +1,16 @@
+package InteligentLife;
+
+public abstract class Puzzle {
+
+	private String description;
+	
+	public Puzzle(String description)
+	{this.description=description;}
+	
+	public abstract boolean solvingPuzzle(String userInput);
+
+	public String getDescription() {
+		return null;
+	}
+	
+}
